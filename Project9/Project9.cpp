b824a867073eaf8897adfab78bc6ab409e34b43d
Danylo <danylo.mysyshyn.ri.2023@lpnu.ua>
2023-10-02 18:40:30 +0300

    finish

--- Project9/Project9.cpp
+++ Project9/Project9.cpp
@@ -1,3 +1,9 @@
+// Lab_03_4.cpp
+// < Мисишин Данило >
+// Лабораторна робота № 3.4
+// Розгалуження, задане плоскою фігурою.
+// Варіант 17
+
 #include<iostream>
 #include<cmath>
 
@@ -12,7 +18,7 @@ int main()
 	cout << "x = "; cin >> x;
 	cout << "y = "; cin >> y;
 
-	if (((y >= 0 && y <= sqrt(R * R - pow(x + R, 2))) && (x >= -R && x <= 0)) || ((y >= 0 && y <= sqrt(R * R - pow(x - R, 2))) && (x >= 0 && x <= R)))
+	if (((y >= 0 && y <= sqrt(R * R - pow(x + R, 2))) && (x >= -R && x <= 0)) || ((y <= 0 && y >= -sqrt(R * R - pow(x - R, 2))) && (x >= 0 && x <= R)))
 		cout << "yes" << endl;
 	else
 		cout << "no" << endl;
